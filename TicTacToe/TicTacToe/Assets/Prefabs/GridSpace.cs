f470ab4d77650f9608487865c5a5ecaa488d1cb3
MikloCO <37591806+MikloCO@users.noreply.github.com>
2018-04-04 16:01:18 +0200

    Scalable UI added
    Added scalable UI

---
+++ TicTacToe/TicTacToe/Assets/Prefabs/GridSpace.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridScript : MonoBehaviour {
+
+	public Button button;
+	public Text buttonText;
+	public string playerSide;
+
+	public void SetSpace ()
+	{
+		buttonText.text = playerSide;
+		button.interactable = false;
+	}
+
+}
